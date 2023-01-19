0244c20ee5e6046ffd8ff463b907d60b8f55bfbb
Abdullah <abdullah.work77@gmail.com>
2023-01-18 21:31:07 -0500

    test

---
+++ src/main/java/com/example/dependencytrack/DependencyTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.dependencytrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dependencytrack/DependencyTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dependencytrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
